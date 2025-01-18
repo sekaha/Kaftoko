b9b8ce8eb88e60cf76c94208fbaba4025a7c4407
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-18 04:42:00 -0600

    nerfed the game, only 6 guesses since there's fewer letters

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  tries: 7, // This changes how many tries you get to finish the game
+  tries: 6, // This changes how many tries you get to finish the game
   language: 'Viossa', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Sekaha', // Put your name here so people know who made this game!
