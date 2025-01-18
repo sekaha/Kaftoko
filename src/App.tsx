eb790025e700e7ea17afbbd509f4043f6d72a18e
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-18 02:32:33 -0600

    changed title color

--- src/App.tsx
+++ src/App.tsx
@@ -146,7 +146,7 @@ const App: React.FC<WithTranslation> = ({ t, i18n }) => {
 
   return (
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div className="flex w-80 mx-auto items-center mb-8 text-pravda_500">
+      <div className="flex w-80 mx-auto items-center mb-8">
         <h1 className="text-xl grow font-bold">
           {t('gameName', { language: CONFIG.language })}
         </h1>
