b7824ee65a86567cf0101774776075b4f2ceb448
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-18 02:44:36 -0600

    Two languages

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -14,6 +14,6 @@ export const CONFIG = {
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 17, 2025 00:00:00', // what date and time to start your game from
   defaultLang: 'vi', // the default interface language
-  availableLangs: ['vi'], // the options available to the user for translation languages
+  availableLangs: ['vi', 'ви'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
 }
