cc926a5773b48913c173212b655002c0e7197307
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-18 01:09:44 -0600

    border update maybe

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -11,10 +11,10 @@ export const Cell = ({ value, status }: Props) => {
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded text-white',
     {
       'bg-black border-slate-700': !status,
-      'border-white': value && !status,
-      'bg-slate-700 text-white border-border-slate-700': status === 'absent',
-      'bg-pravda_500 text-white border-pravda_500': status === 'correct',
-      'bg-uso_500 text-white border-uso_500': status === 'present',
+      'border-slate-800': value && !status,
+      'bg-slate-700 border-border-slate-700': status === 'absent',
+      'bg-pravda_500 border-pravda_500': status === 'correct',
+      'bg-uso_500 border-uso_500': status === 'present',
       'cell-animation': !!value,
     }
   )
