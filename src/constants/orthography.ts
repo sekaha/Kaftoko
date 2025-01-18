cffc371cb4e91f7a0f0bc2ba1ec2100a8af6ea80
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-17 22:05:12 -0600

    changed keyboard ordering

--- src/constants/orthography.ts
+++ src/constants/orthography.ts
@@ -1,27 +1,10 @@
 import { CONFIG } from './config'
 
 export const ORTHOGRAPHY = [
-  'q',
-  'w',
-  'e',
-  'r',
-  't',
-  'y',
-  'u',
-  'i',
-  'o',
-  'p',
-  'a',
-  's',
-  'd',
-  'f',
-  'g',
-  'h',
-  'j',
-  'k',
-  'l',
-  'ø',
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ø', 'z', 'x', 'c', 'v', 'b', 'n', 'm'
 ];
+
+
 
 if (CONFIG.normalization) {
   ORTHOGRAPHY.forEach(
