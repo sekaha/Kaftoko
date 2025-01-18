3a67e0742a1b2efdcdb80c79da43738d4754e338
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-17 23:38:08 -0600

    a  bit of styling in the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        viossa_blue: '#00baff',
+        wrong_red: '#ff8720',
+      },
+    },
   },
   plugins: [],
 }
