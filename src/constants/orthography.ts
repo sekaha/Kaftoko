5d345c28e37a2a820c9c1d15c12ee734fe7035cd
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-17 21:59:09 -0600

    updated order of keys

--- src/constants/orthography.ts
+++ src/constants/orthography.ts
@@ -1,7 +1,26 @@
 import { CONFIG } from './config'
 
 export const ORTHOGRAPHY = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ø'
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  'ø',
 ];
 
 if (CONFIG.normalization) {
