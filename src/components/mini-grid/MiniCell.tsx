15def3323bb3ae074d5d98651a467c28be1c4e39
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-18 01:05:38 -0600

    white text for buttons, hopefully

--- src/components/mini-grid/MiniCell.tsx
+++ src/components/mini-grid/MiniCell.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const MiniCell = ({ status, letter }: Props) => {
   const classes = classnames(
-    'w-10 h-10 border-solid border-2 border-slate-700 flex items-center justify-center mx-0.5 text-lg font-bold rounded',
+    'w-10 h-10 border-solid border-2 border-slate-700 flex items-center justify-center mx-0.5 text-lg font-bold rounded text-white',
     {
       'bg-gray-900': status === 'absent',
       'bg-viossa_500': status === 'correct',
