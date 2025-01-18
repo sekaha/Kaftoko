5093f853e5d1e404918ddd97fdf4c34cff428ef6
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-18 01:18:49 -0600

    color x button

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -51,7 +51,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
             <div className="inline-block align-bottom bg-gray-900 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div className="absolute right-4 top-4">
                 <XCircleIcon
-                  className="h-6 w-6 cursor-pointer"
+                  className="h-6 w-6 cursor-pointer text-red-500"
                   onClick={() => handleClose()}
                 />
               </div>
