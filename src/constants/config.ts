42cbd69d3a1178897f62b1f4ae878c5a2358e00b
Sekaha <davidsommerfield.zg@gmail.com>
2025-01-22 01:43:45 -0600

    set time zone to MST

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -12,7 +12,7 @@ export const CONFIG = {
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: '2025-01-01T00:00:00+06:00', // what date, time, and time zone to start your game from
+  startDate: '2025-01-01T00:00:00-06:00', // what date, time, and time zone to start your game from
   defaultLang: 'vi_la', // the default interface language
   availableLangs: ['vi_la', 'vi_cy', 'vi_ka', 'vi_kr', 'vi_wy', 'vi_pa'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
